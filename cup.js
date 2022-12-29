66993bdceb29730b99006b1416f276841e18a486
Rayyan5 <103158479+Rayyan5@users.noreply.github.com>
2022-12-28 16:10:21 -0800

    Add files via upload

---
+++ cup.js
@@ -0,0 +1,51 @@
+img = "";
+objects = [];
+status = "";
+
+function preload(){
+    img = loadImage('cup.jpg');
+}
+
+function setup() {
+    canvas = createCanvas(640, 420);
+    canvas.center();
+}
+
+
+function draw() {
+
+    if(status != ""){
+        for (i = 0; i < objects.length; i++) {
+          document.getElementById("status").innerHTML = "Status : Object Detected";
+    
+          fill("#FF0000");
+          percent = floor(objects[i].confidence * 100);
+          text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+          noFill();
+          stroke("#FF0000");
+          rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+    }
+
+
+    image(img, 0, 640, 420);
+    fill("#FF0000");
+    text("Cup", 45, 75);
+    noFill();
+    stroke("#FF0000");
+    rect(30, 60, 450, 350 );
+}
+
+function modelLoaded() {
+    console.log("Model Loaded")
+    status = true;
+    objectDetector.detect(img, gotResults);
+}
+
+function gotResult(error, results) {
+    if (error) {
+        console.log(error);
+    }
+
+    console.log(results);
+}
